b1cb994e8bbb020906be8ac8c3310a3cfb4c71df
AndreySavin4 <andreysavin4@ukr.net>
2023-09-10 19:06:48 +0300

    6

--- AwesomeProject/Screens/LoginScreen.js
+++ AwesomeProject/Screens/LoginScreen.js
@@ -1,123 +1,216 @@
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
+import React, { useState, useEffect} from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ImageBackground, 
+  TextInput, 
+  TouchableOpacity, 
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
   Keyboard,
-  ImageBackground,
-} from "react-native";
+  Alert,
+  Platform} from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from "react-redux";
+import bgImage from '../assets/PhotoBg.png';
+import { logIn } from "../redux/auth/authOperations";
+import { selectIsLoggedIn } from "../redux/auth/selectors";
 
-export const LoginScreen = ({ keyboardShown }) => {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+const initialData = {
+  email: '',
+  password: ''
+}
 
-  const onRegSubmit = () => {
-    Keyboard.dismiss();
-    console.log(email, password);
-    setEmail(null);
-    setPassword(null);
-  };
+export const LoginScreen = () => {
+  const [isKeyboardShown, setisKeyboardShown] = useState(false);
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
+  const [formData, setFormData] = useState(initialData);
 
-  const handleEmail = (text) => {
-    setEmail(text);
-  };
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
-  const handlePassword = (text) => {
-    setPassword(text);
-  };
+  console.log('isLoggedIn', isLoggedIn);
 
-  return (
-    // <ImageBackground
-    //   source={require("../assets/bg2x.jpg")}
-    //   style={styles.bgImage}
-    // >
-    <View
-      style={{
-        ...styles.authContainer,
-        paddingBottom: keyboardShown ? 45 : 32,
-        marginBottom: keyboardShown ? -150 : 0,
-      }}
-    >
-      <Text style={styles.authTitle}>Увійти</Text>
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigation.navigate('Home', {
+        screen: 'PostsScreen'
+     });
+    } 
+  }, [isLoggedIn])
 
-      <TextInput
-        style={styles.authInput}
-        placeholder={"Адреса електронної пошти"}
-        onChangeText={handleEmail}
-        value={email}
-      />
-      <TextInput
-        style={styles.authInput}
-        placeholder={"Пароль"}
-        secureTextEntry={true}
-        onChangeText={handlePassword}
-        value={password}
-      />
 
-      <TouchableOpacity style={styles.submitButton} onPress={onRegSubmit}>
-        <Text style={styles.submitButtonText}>Увійти</Text>
-      </TouchableOpacity>
-      <Text style={styles.authLink}>Немає акаунту? Зареєструватися</Text>
-    </View>
-    // </ImageBackground>
-  );
-};
+  const keyBoardHide = () => {
+    Keyboard.dismiss(); 
+    setisKeyboardShown(false);
+  }
+
+    const onSubmit = () => {
+    keyBoardHide();
+    if (!formData.email.includes('@') || formData.email.length < 7) {
+      Alert.alert(
+        "Your email has to include '@' and be more than 7 symbols!!!",
+      );
+    } else if (formData.password.length < 7) {
+      Alert.alert(
+        "Your password's length must be more than 7 symbols !!!",
+      );
+    } else if (formData.password.length < 7 && !formData.email.includes('@')) {
+      Alert.alert(
+        "Please fill in the fields!",
+      );
+    } else {
+      console.log(formData);
+      setFormData(initialData);
+      dispatch(logIn({
+        email: formData.email,
+        password: formData.password}));
+      navigation.navigate('Home', {
+      screen: 'PostsScreen'
+   });
+    }
+  }
+   return (
+    <TouchableWithoutFeedback onPress={keyBoardHide}>
+<View style={styles.container}>
+    <ImageBackground source={bgImage} resizeMode="cover" 
+    style={styles.image}>
+      <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -18 : -18}
+            >
+      <View style={styles.form}>
+        <Text style={styles.header}>Увійти</Text>
+        <TextInput style={styles.textInput} placeholder="Адреса електронної пошти" onFocus={() => setisKeyboardShown(true)}
+        value={formData.email}
+        type="email"a
+        onChangeText={(value) => setFormData((prevState) => ({...prevState, email: value}))}
+        />
+        <TextInput style={styles.textInput} placeholder="Пароль" secureTextEntry={isPasswordSecure} onFocus={() => setisKeyboardShown(true)}
+        value={formData.password}
+        onChangeText={(value) => setFormData((prevState) => ({...prevState, password: value}))}
+        />
+        <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.inputLink}
+        onPress={() => setIsPasswordSecure(!isPasswordSecure)}>
+      <Text style={styles.inputLinkText}>
+      {isPasswordSecure? "Показати" : "Сховати"}
+    </Text>
+    </TouchableOpacity>
+        <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.button}
+        onPress={onSubmit}>
+      <Text style={styles.appButtonText}>
+      Увійти
+    </Text>
+    </TouchableOpacity>
+      <Text style={styles.linkText}>Немає акаунту? </Text>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.linktUnderlined}
+        onPress={() => navigation.navigate("Registration")}>
+      <Text style={styles.linkTextUnderlined}>
+      Зареєструватися
+    </Text>
+    </TouchableOpacity>
+      </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  </View>
+  </TouchableWithoutFeedback>
+    )
+}
 
 const styles = StyleSheet.create({
-  authContainer: {
-    backgroundColor: "#fff",
-    width: "100%",
-    marginTop: "auto",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
-
-  bgImage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-
-  authTitle: {
-    textAlign: "center",
-    marginVertical: 16,
-    letterSpacing: 0.01,
-    fontSize: 30,
-  },
-
-  authInput: {
-    marginTop: 16,
-    marginHorizontal: 16,
-    padding: 16,
-    height: 50,
-    borderRadius: 8,
-    backgroundColor: "#F6F6F6",
-    borderWidth: 1,
-    borderColor: "#E8E8E8",
-  },
-
-  submitButton: {
-    height: 50,
-    backgroundColor: "#FF6C00",
-    color: "#fff",
-    borderRadius: 100,
-    marginHorizontal: 16,
-    marginTop: 43,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  submitButtonText: {
-    fontSize: 16,
-    color: "#fff",
-  },
-
-  authLink: {
-    fontSize: 16,
-    marginTop: 16,
-    marginBottom: 10,
-    textAlign: "center",
-  },
-});
+    container: {
+      flex: 1,
+      position: 'relative',
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+    header: {
+      color: 'black',
+      marginTop: 32,
+      marginBottom: 33,
+      fontSize: 30,
+      lineHeight: 35,
+      fontWeight: '500',
+      textAlign: 'center',
+    },
+    form: {
+      backgroundColor: '#fff',
+      paddingHorizontal: 16,
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      paddingBottom: 144,
+    },
+    textInput: {
+      height: 50,
+      paddingLeft: 16,
+      marginBottom: 16,
+      borderStyle: 'solid',
+      borderColor: '#E8E8E8',
+      borderWidth: 1,
+      borderRadius: 8,
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      shadowColor: '#BDBDBD',
+      shadowOpacity: 0.2,
+    }, 
+    button: {
+      height: 51,
+      marginTop: 27,
+      marginBottom: 16,
+      borderRadius: 100,
+      display: 'flex',
+      alignItems: 'center',
+      paddingHorizontal: 32,
+      paddingVertical: 16,
+      backgroundColor: '#FF6C00',
+    },
+    appButtonText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#fff',
+    },
+    linkText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#1B4371',
+      marginLeft: 62,
+      marginRight: 'auto',
+    },
+    linktUnderlined: {
+        position: 'absolute',
+        left: 198,
+        top: 326,
+        borderBottomWidth: 1,
+        borderBottomColor: '#1B4371',
+    },
+    linkTextUnderlined: {
+        fontSize: 16,
+        fontWeight: '400',
+        lineHeight: 19,
+        color: '#1B4371',
+    },
+    inputLink: {
+      position: 'absolute',
+      right: 32,
+      top: 182,
+    },
+    inputLinkText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#1B4371',
+    }
+  });
--- AwesomeProject/Screens/RegistrationScreen.js
+++ AwesomeProject/Screens/RegistrationScreen.js
@@ -1,152 +1,242 @@
-import { useState, useEffect, useCallback } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
+import React, {useState, useEffect} from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ImageBackground, 
+  TextInput, 
+  Image, 
+  TouchableOpacity, 
+  Alert,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
   Keyboard,
-  ImageBackground,
-} from "react-native";
+  Platform} from "react-native";
+  import { useNavigation } from '@react-navigation/native';
+import bgImage from '../assets/PhotoBg.png';
+import avatar from '../assets/avatar.png';
+import { AntDesign } from '@expo/vector-icons';
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../redux/auth/authOperations";
+import { selectIsLoggedIn } from "../redux/auth/selectors";
 
-export const RegistrationScreen = ({ keyboardShown }) => {
-  const [login, setLogin] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isReady, setIsReady] = useState(false);
 
-  const onRegSubmit = () => {
-    Keyboard.dismiss();
-    console.log(login, email, password);
-    setLogin("");
-    setEmail("");
-    setPassword("");
-  };
+const initialData = {
+  nickName: '',
+  email: '',
+  password: ''
+}
 
-  const handleLogin = (text) => {
-    setLogin(text);
-  };
+export const RegistrationScreen = () => {
 
-  const handleEmail = (text) => {
-    setEmail(text);
-  };
+  const [isKeyboardShown, setisKeyboardShown] = useState(false);
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
+  const [formData, setFormData] = useState(initialData);
 
-  const handlePassword = (text) => {
-    setPassword(text);
-  };
-  return (
-    <View
-      style={{
-        ...styles.authContainer,
-        paddingBottom: keyboardShown ? 45 : 32,
-        marginBottom: keyboardShown ? -150 : 0,
-      }}
-    >
-      <ImageBackground
-        source={require("../assets/bg2x.jpg")}
-        style={styles.bgImage}
-      >
-        <View style={styles.avatarThumb}></View>
-        <Text style={styles.authTitle}>Реєстрація</Text>
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
 
-        <TextInput
-          style={styles.authInput}
-          placeholder={"Логін"}
-          onChangeText={handleLogin}
-          value={login}
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigation.navigate('Home', {
+        screen: 'PostsScreen'
+     });
+    } 
+  }, [isLoggedIn])
+
+  const keyBoardHide = () => {
+    Keyboard.dismiss(); 
+    setisKeyboardShown(false);
+  }
+
+const onSubmit = () => {
+    keyBoardHide();
+    if (!formData.email.includes('@') || formData.email.length < 7) {
+      Alert.alert(
+        "Your email has to include '@' and be more than 7 symbols!!!",
+      );
+    } else if (formData.password.length < 7) {
+      Alert.alert(
+        "Your password's length must be more than 7 symbols !!!",
+      );
+    } else if (formData.password.length < 7 && !formData.email.includes('@')) {
+      Alert.alert(
+        "Please fill in the fields!",
+      );
+    } else if (!formData.nickName) {
+      Alert.alert(
+        "Don't forget your login))",
+      );
+    } else {
+      console.log(formData);
+      setFormData(initialData);
+      dispatch(register({nickName: formData.nickName,
+      email: formData.email,
+      password: formData.password}));
+      navigation.navigate('Home', {
+      screen: 'PostsScreen'
+   });
+    }
+  }
+
+    return (
+        <TouchableWithoutFeedback onPress={keyBoardHide}>
+      <View style={styles.container}>
+        <ImageBackground source={bgImage} resizeMode="cover" 
+        style={styles.image}>
+          <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? -97 : -97}
+            >
+      <View style={styles.form}>
+      <Image
+        style={styles.imageAvatar}
+        // source={avatar}
+      />
+      <View style={styles.svg}>
+      <AntDesign name="pluscircleo" size={25} color="#FF6C00" backgroundColor="#FFFFFF"/>
+      </View>
+        <Text style={styles.header}>Реєстрація</Text>
+        <TextInput style={styles.textInput} placeholder="Логін" onFocus={() => setisKeyboardShown(true)}
+        value={formData.nickName}
+        onChangeText={(value) => setFormData((prevState) => ({...prevState, nickName: value}))}
         />
-        <TextInput
-          style={styles.authInput}
-          placeholder={"Адреса електронної пошти"}
-          onChangeText={handleEmail}
-          value={email}
+        <TextInput style={styles.textInput} placeholder="Адреса електронної пошти" onFocus={() => setisKeyboardShown(true)}
+        value={formData.email}
+        onChangeText={(value) => setFormData((prevState) => ({...prevState, email: value}))}
         />
-        <TextInput
-          style={styles.authInput}
-          placeholder={"Пароль"}
-          secureTextEntry={true}
-          onChangeText={handlePassword}
-          value={password}
+        <TextInput style={styles.textInput} placeholder="Пароль" secureTextEntry={isPasswordSecure} onFocus={() => setisKeyboardShown(true)}
+        value={formData.password}
+        onChangeText={(value) => setFormData((prevState) => ({...prevState, password: value}))}        
         />
-        <TouchableOpacity style={styles.submitButton} onPress={onRegSubmit}>
-          <Text style={styles.submitButtonText}>Зареєстуватися</Text>
-        </TouchableOpacity>
-        <Text style={styles.authLink}>Вже є акаунт? Увійти</Text>
-      </ImageBackground>
-    </View>
-  );
-};
+        <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.inputLink}
+        onPress={() => setIsPasswordSecure(!isPasswordSecure)}>
+      <Text style={styles.inputLinkText}>
+      {isPasswordSecure? "Показати" : "Сховати"}
+    </Text>
+    </TouchableOpacity>
+        <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.button}
+         onPress={onSubmit}
+         >
+      <Text style={styles.appButtonText}>
+      Зареєстуватися
+    </Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.link}
+        onPress={() => navigation.navigate("Login")}>
+      <Text style={styles.linkText}>
+      Вже є акаунт? Увійти
+    </Text>
+    </TouchableOpacity>
+      </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  </View>
+ </TouchableWithoutFeedback>
+
+    )
+}
 
 const styles = StyleSheet.create({
-  bgImage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-
-  authContainer: {
-    backgroundColor: "#fff",
-    width: "100%",
-    marginTop: "auto",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
-
-  avatarThumb: {
-    backgroundColor: "#F6F6F6",
-    width: 120,
-    height: 120,
-    borderRadius: 8,
-    marginTop: -60,
-    marginLeft: "auto",
-    marginRight: "auto",
-  },
-
-  authTitle: {
-    textAlign: "center",
-    marginVertical: 16,
-    letterSpacing: 0.01,
-    fontSize: 30,
-  },
-
-  authInput: {
-    marginTop: 16,
-    marginHorizontal: 16,
-    padding: 16,
-    height: 50,
-    borderRadius: 8,
-    backgroundColor: "#F6F6F6",
-    borderWidth: 1,
-    borderColor: "#E8E8E8",
-  },
-
-  submitButton: {
-    height: 50,
-    backgroundColor: "#FF6C00",
-    color: "#fff",
-    borderRadius: 100,
-    marginHorizontal: 16,
-    marginTop: 43,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  submitButtonText: {
-    fontSize: 16,
-    color: "#fff",
-  },
-
-  authLink: {
-    fontSize: 16,
-    marginTop: 16,
-    marginBottom: 10,
-    textAlign: "center",
-  },
-
-  inputLinkText: {
-    fontSize: 16,
-    fontWeight: "400",
-    lineHeight: 19,
-    color: "#1B4371",
-  },
-});
+    container: {
+      flex: 1,
+      position: 'relative',
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+    header: {
+      color: 'black',
+      marginTop: 92,
+      marginBottom: 32,
+      fontSize: 30,
+      lineHeight: 35,
+      fontWeight: '500',
+      textAlign: 'center',
+    },
+    form: {
+      backgroundColor: '#fff',
+      paddingHorizontal: 16,
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      paddingBottom: 45,
+    },
+    textInput: {
+      height: 50,
+      paddingLeft: 16,
+      marginBottom: 16,
+      borderStyle: 'solid',
+      borderColor: '#E8E8E8',
+      borderWidth: 1,
+      borderRadius: 8,
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      shadowColor: '#BDBDBD',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+        width: 0,
+        height: 4
+      }
+    }, 
+    button: {
+      height: 51,
+      marginTop: 27,
+      marginBottom: 16,
+      borderRadius: 100,
+      display: 'flex',
+      alignItems: 'center',
+      paddingHorizontal: 32,
+      paddingVertical: 16,
+      backgroundColor: '#FF6C00',
+    },
+    appButtonText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#fff',
+    },
+    linkText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#1B4371',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    imageAvatar: {
+      width: 120,
+      height: 120,
+      position: 'absolute',
+      alignSelf: 'center',
+      top: -60,
+      backgroundColor: '#F6F6F6',
+      borderRadius: 16,
+    },
+    svg: {
+      position: 'absolute',
+      width: 25,
+      height: 25,
+      left: 235,
+      top: 21,
+    },
+    inputLink: {
+      position: 'absolute',
+      right: 32,
+      top: 308,
+    },
+    inputLinkText: {
+      fontSize: 16,
+      fontWeight: '400',
+      lineHeight: 19,
+      color: '#1B4371',
+    }
+  });
